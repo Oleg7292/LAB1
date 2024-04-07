5b2ddc93affd6c80d887b016e1efa1c826d4f202
Maxioner <m4xskachkov@yandex.ru>
2024-04-07 23:13:16 +0700

    new

--- file.cpp
+++ file.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
-
 // Функция для вычисления периметра прямоугольника
 int Perimetr(int a, int b) {
     return 2 * (a + b);
@@ -18,16 +17,20 @@ int Diagonal(float a, float b) {
 }
 
 int main() {
-    setlocale(LC_ALL, "Rus");
+    setlocale(LC_ALL, "Russian");
     float a, b;
     cout << "Введите ширину прямоугольника: ";
     cin >> a;
     cout << "Введите высоту прямоугольника: ";
     cin >> b;
-
-    cout << "Периметр прямоугольника: " << Perimetr(a, b) << endl;
-    cout << "Площадь прямоугольника: " << Ploshad(a, b) << endl;
-    cout << "Длина диагонали прямоугольника: " << Diagonal(a, b) << endl;
+    if (a <= 0 || b <= 0) {
+        cout << "Неверные стороны" << endl;
+        }
+    else {
+        cout << "Периметр прямоугольника: " << Perimetr(a, b) << endl;
+        cout << "Площадь прямоугольника: " << Ploshad(a, b) << endl;
+        cout << "Длина диагонали прямоугольника: " << Diagonal(a, b) << endl;
+    }
 
     return 0;
 }
