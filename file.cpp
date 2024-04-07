25a88ff366bedf359554c1775d96ebc291c1c70a
Maxioner <m4xskachkov@yandex.ru>
2024-04-07 22:59:53 +0700

    test

---
+++ file.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+// Функция для вычисления периметра прямоугольника
+int Perimetr(int a, int b) {
+    return 2 * (a + b);
+}
+
+// Функция для вычисления площади прямоугольника
+int Ploshad(float a, float b) {
+    return a * b;
+}
+
+// Функция для вычисления длины диагонали прямоугольника
+int Diagonal(float a, float b) {
+    return sqrt(a * a + b * b);
+}
+
+int main() {
+    setlocale(LC_ALL, "Rus");
+    float a, b;
+    cout << "Введите ширину прямоугольника: ";
+    cin >> a;
+    cout << "Введите высоту прямоугольника: ";
+    cin >> b;
+
+    cout << "Периметр прямоугольника: " << Perimetr(a, b) << endl;
+    cout << "Площадь прямоугольника: " << Ploshad(a, b) << endl;
+    cout << "Длина диагонали прямоугольника: " << Diagonal(a, b) << endl;
+
+    return 0;
+}
