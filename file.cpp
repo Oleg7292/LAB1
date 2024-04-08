e1f101dd985f44a33a8e8cbb97fb1a3eccac342f
Maxioner <m4xskachkov@yandex.ru>
2024-04-08 10:43:37 +0700

    newv

--- file.cpp
+++ file.cpp
@@ -19,17 +19,17 @@ int Diagonal(float a, float b) {
 int main() {
     setlocale(LC_ALL, "Russian");
     float a, b;
-    cout << "Введите ширину прямоугольника: ";
+    cout << "Write width: ";
     cin >> a;
-    cout << "Введите высоту прямоугольника: ";
+    cout << "Write height: ";
     cin >> b;
     if (a <= 0 || b <= 0) {
-        cout << "Неверные стороны" << endl;
+        cout << "Wrong sides" << endl;
         }
     else {
-        cout << "Периметр прямоугольника: " << Perimetr(a, b) << endl;
-        cout << "Площадь прямоугольника: " << Ploshad(a, b) << endl;
-        cout << "Длина диагонали прямоугольника: " << Diagonal(a, b) << endl;
+        cout << "Perimetr: " << Perimetr(a, b) << endl;
+        cout << "Ploshad: " << Ploshad(a, b) << endl;
+        cout << "Diagonal: " << Diagonal(a, b) << endl;
     }
 
     return 0;
